cfb29a499bad382d353e3292630dce9a08669d89
rag16c <44474646+rag16c@users.noreply.github.com>
2019-04-09 19:54:33 -0400

    Changed player sprite
    Changed animation for 4 directions
    Update animation model for the character
    Added boundary and collision check for player

--- scripts/game/player.js
+++ scripts/game/player.js
@@ -1,5 +1,9 @@
 var playerImage = new Image();
-playerImage.src = "images/spritesheets/playertest.png";
+playerImage.src = "maps/character.png";
+
+var moveAFrame = 9;
+var swordAFrame = 6;
+var startWalk = 8;
 
 function initPlayer(options) {
 	var that = {};
@@ -7,30 +11,41 @@ function initPlayer(options) {
 	that.X = options.X;
 	that.Y = options.Y;
 	that.aFrame = options.aFrame;
+	that.action = "stand";
+	that.direction = 3;  // 0 = up, 1 = left, 2 = down, 3 = right
     that.image = playerImage;
 
 	that.draw = function(ctx) {
-		ctx.drawImage(that.image,256*that.aFrame,0,256,256,that.X,that.Y,256,256);
+		ctx.drawImage(that.image,63*that.aFrame,63*(startWalk+that.direction),63,63,that.X,that.Y,126,126);
 	};
 	
-	that.moveCheck = function(Up,Down,Left,Right) {
-		if (Up)
+	that.moveCheck = function(Up,Down,Left,Right,width,height) {
+		if (Up && that.Y-5 >= -28)
 			that.Y-=5;
-		if (Down)
+		if (Down && that.Y+5 <= height-128)
 			that.Y+=5;
-		if (Left)
+		if (Left && that.X-5 >= -28)
 			that.X-=5;
-		if (Right)
+		if (Right && that.X+5 <= width-100) 
 			that.X+=5;
+			
+		if (Right) 
+			that.direction = 3;
+		else if (Left) 
+			that.direction = 1;
+		else if (Down)
+			that.direction = 2;
+		else if (Up)
+			that.direction = 0;
 		
-		if (Up || Down || Right || Left ) {
+		if ( Up || Right || Down || Left ) {
 			that.aFrame++;
-			if ( that.aFrame > 5)
+			if ( that.aFrame == 9 )
 				that.aFrame = 0;
+			
 		}
 		else
 			that.aFrame = 0;
-			
 	};
 	
 	return that;
