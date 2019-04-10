c319f96b5cc55e78e220697cedc8ab81a828cea5
rgkenney <rgk16b@my.fsu.edu>
2019-04-10 14:43:25 -0400

    Replaced box with enemy model

--- scripts/game/enemy.js
+++ scripts/game/enemy.js
@@ -1,17 +1,19 @@
+var enemyImage= new Image();
+enemyImage.src = "images/spritesheets/skeleton_sprites.png";
+
 function initEnemy(options) {
 	var that = {};
 	//initialize Enemy variables
 	that.X = 500;
 	that.Y = 300;
 	that.health = 100;
-	that.length = 252;
+	that.length = 100;
 	that.death = false;
 	
 	//Call the draw function to create basic enemy rectangle
 	that.draw = function(ctx) {
 		if ( that.death == false ) {
-		ctx.fillStyle = "#FF0000";
-		ctx.fillRect(500,300,that.length,that.length); 
+			ctx.drawImage(enemyImage,0,128,64,64,that.X,that.Y,128,128);
 		}
 	};
 	
