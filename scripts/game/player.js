5fd3733a9cd4e7393db0a7be59db75e3a42e1126
rag16c <44474646+rag16c@users.noreply.github.com>
2019-04-09 23:28:34 -0400

    Added Collision for sword
    Added alot of math
    Added detection between circles and rectangle
    Only detect collision left and right, and on last 3 frames of sword animation
    Seemed useless to do so
    Included attack function

--- scripts/game/player.js
+++ scripts/game/player.js
@@ -16,12 +16,17 @@ function initPlayer(options) {
 	that.whichAction = "stand";
 	that.direction = 3;  // 0 = up, 1 = left, 2 = down, 3 = right
     that.image = playerImage;
+	that.standRight = that.X + 90;
+	that.standLeft = that.X +40;
+	that.standUp = that.Y + 20;
+	that.standDown = that.Y + 125;
 	that.health = 100;
 	that.weapon = "shortSword";
 	
-	
 	that.draw = function(ctx) {
 		ctx.drawImage(that.image,63*that.aFrame,63*(action+that.direction),63,63,that.X,that.Y,126,126);
+		ctx.fillStyle = "#00FF00";
+		ctx.fillRect(that.X+13,that.Y+63,5,5);
 	};
 	
 	that.moveCheck = function(Up,Down,Left,Right,width,height) {
@@ -48,6 +53,11 @@ function initPlayer(options) {
 			that.aFrame++;
 			if ( that.aFrame == 9 )
 				that.aFrame = 0;
+		
+		that.standRight = that.X + 90;
+		that.standLeft = that.X +40;
+		that.standUp = that.Y + 20;
+		that.standDown = that.Y + 125;		
 			
 		}
 		else
@@ -60,9 +70,113 @@ function initPlayer(options) {
 		action = startSword;
 		that.aFrame = -1;
 		setTimeout(animateAttack,0,that)
-	}
+	};
+	
+	that.collisionCheck = function(Enemy) {
+		// Check X collision
+		if ( that.standRight >= Enemy.X && that.standLeft <= Enemy.X+Enemy.length ) {
+			// Check Y collision
+			if ( that.standDown >= Enemy.Y && that.standUp <= Enemy.Y + Enemy.length) {
+			}
+		}
+		
+		if ( swordCollision(that,Enemy) == true && Enemy.death == false ) {
+			Enemy.health -= 20;
+			Enemy.checkDeath();
+			alert(Enemy.health);
+		}
+	};
 	
 	return that;
+}
+
+function swordCollision(that,Enemy) {
+	if ( that.aFrame == 3 && that.whichAction == "attack" ) {
+		 if ( that.direction == 1 ) {
+			  theX = that.X+22;
+			  theY = that.Y+95;
+		}
+		else if ( that.direction == 3 ) {
+			  theX = that.X-22+126;
+			  theY = that.Y+95;
+		}
+		else
+			  return false;
+		  
+		return collisionMath(theX,theY,Enemy);
+	}
+	else if ( that.aFrame == 4 && that.whichAction == "attack" ) {
+		if ( that.direction == 1 ) {
+			theX = that.X+10
+			theY = that.Y+68;
+		}
+		else if ( that.direction == 3 ) {
+			theX = that.X-10+126;
+			theY = that.Y+68;
+		}
+		else	
+			return false;
+		
+		return collisionMath(theX,theY,Enemy);
+	}
+	else if ( that.aFrame == 5 && that.whichAction == "attack" ) {
+		if ( that.direction == 1 ) {
+			theX = that.X +13;
+			theY = that.Y +63;
+		}
+		else if ( that.direction == 3 ) {
+			theX = that.X-13+126;
+			theY = that.Y+63;
+		}
+		else 
+			return false;
+		
+		return collisionMath(theX,theY,Enemy);
+	}
+
+	return false;
+}
+
+function collisionMath(theX,theY,Enemy) {
+	  if ( theX <=Enemy.X+Enemy.length && theX >=Enemy.X){
+		 if ( theY <= Enemy.Y+Enemy.length && theY>=Enemy.Y)
+			return true;
+		 else {
+			if ( Math.abs(theY - Enemy.Y) < Math.abs(theY-Enemy.Y-Enemy.length))
+				y = Math.abs(theY-Enemy.Y);
+			else	
+				y = Math.abs(theY-Enemy.Y-Enemy.length);
+			
+			if ( Math.sqrt(y*y) <= 30 )
+				return true;
+		}
+	  }
+	 else if ( theY <= Enemy.Y+Enemy.length && theY >= Enemy.Y ) {
+		if ( Math.abs(theX - Enemy.X) < Math.abs(theX - Enemy.X - Enemy.length))
+			x = Math.abs(theX -Enemy.X);
+		else
+			x = Math.abs(theX-Enemy.X-Enemy.length);
+		
+		if ( Math.sqrt(x*x) <= 15 )
+			return true;
+	  }	
+	else if ( theX > Enemy.X+Enemy.length || theX < Enemy.X ){
+		if ( Math.abs(theX - Enemy.X) < Math.abs(theX - Enemy.X - Enemy.length))
+			x = Math.abs(theX -Enemy.X);
+		else
+			x = Math.abs(theX-Enemy.X-Enemy.length);
+		
+		if ( Math.abs(theY - Enemy.Y) < Math.abs(theY-Enemy.Y-Enemy.length))
+			y = Math.abs(theY-Enemy.Y);
+		else	
+			y = Math.abs(theY-Enemy.Y-Enemy.length);
+	
+		if ( Math.sqrt(x*x + y*y) <= 30 )
+			return true;
+	  }
+	
+	return false;
+	
 }
 
 function animateAttack(that) {
