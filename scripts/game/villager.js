a60ff8fadfb0a119b150b8e98868d4d7468b2ed1
rag16c <44474646+rag16c@users.noreply.github.com>
2019-04-20 14:38:23 -0400

    Update villager.js

--- scripts/game/villager.js
+++ scripts/game/villager.js
@@ -2,7 +2,13 @@
 
 function initVillager(options) {
   var that = {};
+  that.startX = 0;
+  that.startY = 0;
+  that.endX = 0;
+  that.endY = 0;
 
-
-
+  that.interaction = function(options) {
+    
+  }
+  
 }
