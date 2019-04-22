3f9123bc6396401098b2da2a8e70d8728646db76
rag16c <44474646+rag16c@users.noreply.github.com>
2019-04-22 14:17:15 -0400

    Added draw function

--- scripts/game/villager.js
+++ scripts/game/villager.js
@@ -2,17 +2,18 @@
 
 function initVillager(options) {
   var that = {};
-  that.startX = 0;
-  that.startY = 0;
-  that.endX = 0;
-  that.endY = 0;
+  that.startX = 2000;
+  that.startY = 1000;
+  that.endX = 2050;
+  that.endY = 1050;
 
-  that.draw() {
-   // draw villager here 
-  }
+  that.draw() = function(ctx) {
+    ctx.fillStyle = "#FF0000";
+    ctx.fillRect(2000,1000,50,50);
+  };
   
   that.interaction = function(options) {
     // Call text box and insert string into it
-  }
+  };
   
 }
