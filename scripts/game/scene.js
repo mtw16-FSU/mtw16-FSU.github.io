e0589b629cfacbd56537e7b2bc8a258f00d57403
rag16c <44474646+rag16c@users.noreply.github.com>
2019-04-22 16:15:29 -0400

    Update scene.js

--- scripts/game/scene.js
+++ scripts/game/scene.js
@@ -328,7 +328,7 @@ function drawTextBox(sentence) {
 	ctx.fillRect(width*.10,height*.70,width*.80,height*.20);
 	ctx.font = "60px Sniglet";
 	ctx.fillStyle = "#000000";
-	ctx.fillText("hello son",width*.11,height*.78);
+	ctx.fillText("WWWWWWWWWW WWWWWWWWWW WWWWWWWWWW",width*.11,height*.78);
 	document.onkeydown = null;
 	document.onkeyup = null;
 	document.onkeydown = textHandler;
