d619c50cd7f502067e3025f8542174766db386ed
rag16c <44474646+rag16c@users.noreply.github.com>
2019-04-23 21:08:52 -0400

    Update player.js

--- scripts/game/player.js
+++ scripts/game/player.js
@@ -165,8 +165,8 @@ function changeIBox(dir,iBox,X,Y) {
 }
 
 function drawHealth(Object) {
-	ctx.fillStyle = "#000000";
-	ctx.rect(Object.X,Object.Y,128,10);
+	//ctx.fillStyle = "#000000";
+	//ctx.rect(Object.X,Object.Y,128,10);
 	ctx.fillStyle = "#00FF00";
 	ctx.fillRect(Object.X,Object.Y,128*(Object.health/Object.totalHealth),10);
 }
