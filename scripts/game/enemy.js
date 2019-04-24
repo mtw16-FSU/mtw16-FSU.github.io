6d3169114492f6fe7cbbf93db77b0375c8b74806
rag16c <44474646+rag16c@users.noreply.github.com>
2019-04-24 16:13:13 -0400

    Attempted to add enemy movement

--- scripts/game/enemy.js
+++ scripts/game/enemy.js
@@ -1,10 +1,17 @@
 var enemyImage = new Image();
 enemyImage.src = "images/spritesheets/skeleton_sprites.png";
-var enemyAnimation;
 
 function initEnemy(options) {
 	var that = {};
 	//initialize Enemy variables
+	that.enemyAnimation;
+	that.startWalk = 0;
+	that.startDeath = 12;
+	that.moveAFrame = 9;
+	that.deathAFrame = 6;
+	that.aFrame = 0;
+	that.direction = 0; // up,left,down,right
+	that.action = 0;
 	that.xOff = 40; //34
 	that.yOff = 20; // 30
 	that.X = 500+that.xOff;
@@ -20,7 +27,7 @@ function initEnemy(options) {
 	//Call the draw function to create basic enemy rectangle
 	that.draw = function() {
 		if ( that.death == false ) {
-			ctx.drawImage(enemyImage,0,128,64,64,that.X+(dx/8)*64-that.xOff,that.Y+(dy/8)*64-that.yOff,128,128);
+			ctx.drawImage(enemyImage,64*that.aFrame,64*(that.directon+that.action),64,64,that.X+(dx/8)*64-that.xOff,that.Y+(dy/8)*64-that.yOff,128,128);
 			drawHealth(that);
 		}
 	};
@@ -34,24 +41,57 @@ function initEnemy(options) {
 	return that;
 }
 
-function basicEnemyAI() {
+function basicEnemyAI(Enemy) {
 	if(Enemy.time + 800 < Date.now()){
 		if(Enemy.time + 1600 < Date.now()){
 			Enemy.time = Date.now();
 		}
-		if ( Player.standRight < Enemy.X +(dx/8)*64+Enemy.lengthX) 
+		if ( Player.standRight < Enemy.X +(dx/8)*64+Enemy.lengthX) {
 			Enemy.X-=2;
-		if ( Player.standLeft > Enemy.X + (dx/8)*64)
+			Enemy.direction = 1;
+		}
+		if ( Player.standLeft > Enemy.X + (dx/8)*64) {
 			Enemy.X+=2;
+			Enemy.direction = 3;
+		}
 	
-		if ( Player.standDown < Enemy.Y + (dy/8)*64+Enemy.lengthY)
+		if ( Player.standDown < Enemy.Y + (dy/8)*64+Enemy.lengthY) {
 			Enemy.Y-=2;
-		if ( Player.standUp > Enemy.Y + (dy/8)*64)
+			Enemy.direction = 0;
+		}
+		if ( Player.standUp > Enemy.Y + (dy/8)*64) {
 			Enemy.Y+=2;
+			Enemy.direction = 2;
+		}
 	}
 	
-	if ( Enemy.death == false )
-		enemyAnimation = requestAnimationFrame(basicEnemyAI);
-	else		
-    		cancelAnimationFrame(enemyAnimation);
+		if ( Enemy.aFrame == Enemy.moveAFrame )
+			Enemy.aFrame = 0;
+	
+	if ( Enemy.health > 0 )
+		Enemy.enemyAnimation = requestAnimationFrame(function(Enemy) {basicEnemyAI(Enemy)};);
+	else	{	
+    		cancelAnimationFrame(Enemy.enemyAnimation);
+		{
+		Enemy.aFrame = 0;
+		Enemy.action = 12;
+		enemyDeath(Enemy);
+	}
+}
+
+function enemyDeath(Enemy) {
+	Enemy.aFrame++;
+	if ( Enemy.aFrame == Enemy.deathAFrame ) {
+		Enemy.aFrame = 0;
+		Enemy.death = true;
+	}
+	else
+		Enemy.enemyAnimation = requestAnimationFrame(function(Enemy){enemyDeath(Enemy)};);
+	
+}
+	
+	
+	
+	
+	
 }
