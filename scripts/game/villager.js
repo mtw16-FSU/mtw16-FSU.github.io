6b5cf730bee42d5f02a44fbe43c443724079825c
rag16c <44474646+rag16c@users.noreply.github.com>
2019-04-23 21:29:57 -0400

    Update villager.js

--- scripts/game/villager.js
+++ scripts/game/villager.js
@@ -5,8 +5,8 @@ function initVillager(options) {
   var that = {};
   that.xLength = 60;
   that.yLength = 96;
-  that.startX = 2000+that.xOff;
-  that.startY = 1000+that.yOff; 
+  that.startX = 2000;
+  that.startY = 1000; 
   that.endX = that.startX+that.xLength;
   that.endY = that.startY+that.yLength;
   that.drawText = false;
@@ -16,8 +16,6 @@ function initVillager(options) {
     ctx.drawImage(villagerImage,0,128,64,64,that.startX+(dx/8)*64-40,that.startY+(dy/8)*64-20,128,128);
     that.endX = that.startX + (dx/8)*64+that.xLength;
     that.endY = that.startY + (dy/8)*64+that.yLength;
-    ctx.filStyle = "#000000";
-    ctx.fillRect(that.startX+(dx/8)*64,that.startY+(dy/8)*64,128,128);
   };
   
   that.interaction = function(options) {
