3af03be391de9acc12bfe0a9b56248608353f849
rag16c <44474646+rag16c@users.noreply.github.com>
2019-04-23 20:14:48 -0400

    Updated villager boundaries

--- scripts/game/villager.js
+++ scripts/game/villager.js
@@ -3,15 +3,15 @@ villagerImage.src = "images/spritesheets/villager.png";
 
 function initVillager(options) {
   var that = {};
-  that.startX = 2000;
-  that.startY = 1000;
-  that.endX = 2100;
-  that.endY = 1100;
+  that.startX = 2000+17;
+  that.startY = 1000+14; 
+  that.endX = 2047;
+  that.endY = 1062;
   that.drawText = false;
   that.sentence = "1234567890 1234567890 1234567890 1234567890 1234567890 1234567890 1234567890 ";
   
   that.draw = function() {
-    ctx.drawImage(villagerImage,0,128,64,64,that.startX+(dx/8)*64-40,that.startY+(dy/8)*64-20,128,128);
+    ctx.drawImage(villagerImage,0,128,64,64,that.startX-17+(dx/8)*64-40,that.startY-14+(dy/8)*64-20,128,128);
     that.endX = that.startX + (dx/8)*64 + 100;
     that.endY = that.startY + (dy/8)*64 + 100; 
   };
