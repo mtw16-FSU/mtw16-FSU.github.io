df66b15fa84682f7f96967cbe8866a8032898ecc
rag16c <44474646+rag16c@users.noreply.github.com>
2019-04-23 20:23:13 -0400

    Update code for villager

--- scripts/game/villager.js
+++ scripts/game/villager.js
@@ -3,15 +3,19 @@ villagerImage.src = "images/spritesheets/villager.png";
 
 function initVillager(options) {
   var that = {};
-  that.startX = 2000+17;
-  that.startY = 1000+14; 
-  that.endX = 2047;
-  that.endY = 1062;
+  that.xOff = 34;
+  that.yOff = 28;
+  that.xLength = 60;
+  that.yLength = 96;
+  that.startX = 2000+that.xOff;
+  that.startY = 1000+that.yOff; 
+  that.endX = that.startX+that.xLength;
+  that.endY = that.startY+that.yLength;
   that.drawText = false;
   that.sentence = "1234567890 1234567890 1234567890 1234567890 1234567890 1234567890 1234567890 ";
   
   that.draw = function() {
-    ctx.drawImage(villagerImage,0,128,64,64,that.startX-17+(dx/8)*64-40,that.startY-14+(dy/8)*64-20,128,128);
+    ctx.drawImage(villagerImage,0,128,64,64,that.startX-that.xOff+(dx/8)*64-40,that.startY-that.yOff+(dy/8)*64-20,128,128);
     that.endX = that.startX + (dx/8)*64 + 100;
     that.endY = that.startY + (dy/8)*64 + 100; 
   };
