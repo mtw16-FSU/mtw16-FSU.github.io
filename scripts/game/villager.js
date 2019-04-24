13018621f8f1757eb2719728acfb21deed6fac17
miketesfaye <47304658+miketesfaye@users.noreply.github.com>
2019-04-23 22:29:58 -0400

    Updated Villager textbook
    Villager begins the main questline and instructs user on how to progress through the game.
    
    Committed with Riley

--- scripts/game/villager.js
+++ scripts/game/villager.js
@@ -10,7 +10,7 @@ function initVillager(options) {
   that.endX = that.startX+that.xLength;
   that.endY = that.startY+that.yLength;
   that.drawText = false;
-  that.sentence = "1234567890 1234567890 1234567890 1234567890 1234567890 1234567890 1234567890 ";
+  that.sentence = "My Lord! The prophecies heralded your return. Your path to take back your throne begins now, sire. Your rivals stand in your way, once you defeat them, you may leave this province in the top right and head towards the castle!";
   
   that.draw = function() {
     ctx.drawImage(villagerImage,0,128,64,64,that.startX+(dx/8)*64-40,that.startY+(dy/8)*64-20,128,128);
