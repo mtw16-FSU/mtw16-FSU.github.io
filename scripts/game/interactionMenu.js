9d97a198a28124485acde625acc307262328a7bb
rag16c <44474646+rag16c@users.noreply.github.com>
2019-04-28 15:05:03 -0400

    Update interactionMenu.js

--- scripts/game/interactionMenu.js
+++ scripts/game/interactionMenu.js
@@ -27,12 +27,12 @@ function drawIMenu() {
     for(i = 0; i < options.length; i+=2 ){
 	ctx.fillStyle = "black";
 	if ( i == 0 ) {
-  	   ctx.fillText("Weapons:",width*.13,height*((.90-.075*options.length+0.06)+0.06*(i/2 + j)));	    
+  	   ctx.fillText("Weapons:",width*.13,height*((.90-.075*options.length)+0.06*(i/2 + j)));	    
     	   j++;
     	}
 	else if ( options[i] == "potion" ) {
 	   j++;
-	   ctx.fillText("Consumables:",width*.13,height*((.90-.075*options.length+0.06)+0.06*(i/2 + j)));	
+	   ctx.fillText("Consumables:",width*.13,height*((.90-.075*options.length)+0.06*(i/2 + j)));	
 	   j++;
 	}
 	else if ( i+1 == options.length )
@@ -43,10 +43,10 @@ function drawIMenu() {
        else 
            ctx.fillStyle = "black";
       
-        ctx.fillText(options[i],width*.13,height*((.90-.075*options.length+0.06)+0.06*(i/2 + j)));
+        ctx.fillText(options[i],width*.13,height*((.90-.075*options.length)+0.06*(i/2 + j)));
 	if ( (drawShop == true || Player.drawInv == true) && i+1 != options.length ) {
 	  ctx.fillStyle = "black";
-	  ctx.fillText(options[i+1],width*.41,height*((.90-.075*options.length+0.06)+0.06*(i/2 + j)));
+	  ctx.fillText(options[i+1],width*.41,height*((.90-.075*options.length)+0.06*(i/2 + j)));
 	}
 	    
 	if ( i+3 == options.length && Player.drawInv == true ) {
