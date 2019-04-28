90eacc338a394a8c7826ac6644cd509aaa7a91fb
mtw16-FSU <47019474+mtw16-FSU@users.noreply.github.com>
2019-04-28 13:34:38 -0400

    Added villager dialogue box

--- scripts/game/scene.js
+++ scripts/game/scene.js
@@ -450,12 +450,13 @@ function initTextBox(i) {
 function drawTextBox(sentence,position) {
 	if ( sentence.length != 0 ) {
 	   ctx.fillStyle = "#FFFFFF";
-	   ctx.fillRect(width*.10,height*.70,width*.80,height*.20);
+	   ctx.drawImage(interactionMenuImage, width*.09, height*.70, width*.84, height*.20);	   
+	   //ctx.fillRect(width*.10,height*.70,width*.80,height*.20);
 	   ctx.font = "60px Sniglet";
 	   ctx.fillStyle = "#000000";
-	   ctx.fillText(sentence.substring(position*60,position*60+60),width*.11,height*.78);
+	   ctx.fillText(sentence.substring(position*60,position*60+60),width*.12,height*.78);
 	if ( sentence.length > position*60+60 )
-	   ctx.fillText(sentence.substring(position*60+60,position*60+120),width*.11,height*(.86));
+	   ctx.fillText(sentence.substring(position*60+60,position*60+120),width*.12,height*(.86));
 	}
 }
 
