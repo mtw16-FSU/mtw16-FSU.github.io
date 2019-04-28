69fea6910fed74f04ad5a5aae91d221ca82e96b4
mtw16-FSU <47019474+mtw16-FSU@users.noreply.github.com>
2019-04-28 13:39:05 -0400

    Added villager dialogue box

--- scripts/game/interactionMenu.js
+++ scripts/game/interactionMenu.js
@@ -16,19 +16,20 @@ function initIMenu(i) {
 function drawIMenu() {
   if ( Player.drawInv == true || drawShop == true ) {
     ctx.fillStyle = "#FFFFFF";
-    ctx.fillRect(width*.10,height*(.90-.075*(Math.ceil(options.length/2)+4)),width*.50,height*(.075*(Math.ceil(options.length/2)+4)));
+    ctx.drawImage(interactionMenuImage, width*.10, height*(.90-.075*(Math.ceil(options.length/2)+4)), width*.80, height*(.075*(Math.ceil(options.length/2)+4)));
+    //ctx.fillRect(width*.10,height*(.90-.075*(Math.ceil(options.length/2)+4)),width*.50,height*(.075*(Math.ceil(options.length/2)+4)));
     ctx.font = "45px Sniglet";
   
     var j = 0;
     for(i = 0; i < options.length; i+=2 ){
 	ctx.fillStyle = "black";
 	if ( i == 0 ) {
-  	   ctx.fillText("Weapons:",width*.11,height*((.90-.075*options.length+0.06)+0.06*(i/2 + j)));	    
+  	   ctx.fillText("Weapons:",width*.13,height*((.90-.075*options.length+0.06)+0.06*(i/2 + j)));	    
     	   j++;
     	}
 	else if ( options[i] == "potion" ) {
 	   j++;
-	   ctx.fillText("Consumables:",width*.11,height*((.90-.075*options.length+0.06)+0.06*(i/2 + j)));	
+	   ctx.fillText("Consumables:",width*.13,height*((.90-.075*options.length+0.06)+0.06*(i/2 + j)));	
 	   j++;
 	}
 	else if ( i+1 == options.length )
@@ -39,7 +40,7 @@ function drawIMenu() {
        else 
            ctx.fillStyle = "black";
       
-        ctx.fillText(options[i],width*.11,height*((.90-.075*options.length+0.06)+0.06*(i/2 + j)));
+        ctx.fillText(options[i],width*.13,height*((.90-.075*options.length+0.06)+0.06*(i/2 + j)));
 	if ( drawShop == true && i+1 != options.length ) {
 	  ctx.fillStyle = "black";
 	  ctx.fillText(options[i+1],width*.41,height*((.90-.075*options.length+0.06)+0.06*(i/2 + j)));
@@ -48,7 +49,8 @@ function drawIMenu() {
   }
   else {
     ctx.fillStyle = "#FFFFFF";
-    ctx.fillRect(width*.10,height*(.90-.10*options.length),width*.80,height*(.10*options.length));
+    ctx.drawImage(interactionMenuImage, width*.10, height*(.90-.10*options.length), width*.80, height*(.10*options.length));
+    //ctx.fillRect(width*.10,height*(.90-.10*options.length),width*.80,height*(.10*options.length));
     ctx.font = "60px Sniglet";
   
     for(i = 0; i < options.length; i++ ){
@@ -57,7 +59,7 @@ function drawIMenu() {
        else 
            ctx.fillStyle = "black";
       
-        ctx.fillText(options[i],width*.11,height*((.90-.10*options.length+0.08)+0.08*i));
+        ctx.fillText(options[i],width*.13,height*((.90-.10*options.length+0.08)+0.08*i));
     }
   }
 }
